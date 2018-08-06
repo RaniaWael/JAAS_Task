b75dc3b1f1fa9079222a6c29f8d90baeffb4b543
RaniaWael <rania.waelsameh@yahoo.com>
2018-08-06 17:35:35 +0200

    Adding new Employee is the only thing still not working

--- src/main/java/org/wildfly/swarm/examples/jaas/basic/EmployeeResource.java
+++ src/main/java/org/wildfly/swarm/examples/jaas/basic/EmployeeResource.java
@@ -1,18 +1,21 @@
 package org.wildfly.swarm.examples.jaas.basic;
 
+import org.springframework.test.annotation.Rollback;
+
 import javax.enterprise.context.RequestScoped;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-import javax.transaction.UserTransaction;
-import javax.ws.rs.*;
+import javax.persistence.Query;
+import javax.transaction.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-
-import static java.util.logging.Level.SEVERE;
 
 /**
  * @author Ken Finnigan
@@ -37,61 +40,53 @@ public class EmployeeResource {
 
     @GET
     @Path("getAllEmployees")
-    public Response getAll() {
-        return Response.ok().
-                entity(em.createQuery("SELECT s FROM Employee s", Employee.class).getResultList()).build();
+    public Employee[] getAll() {
+        return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
     }
 
     @GET
     @Path("addNewEmp")
-    @Transactional
-    public Response addEmp() {
-        try {
-            UserTransaction transaction = (UserTransaction)new InitialContext().lookup("java:comp/UserTransaction");
-            transaction.begin();
-            Employee newEmp = new Employee();
-            newEmp.setName("Anwar");
-            newEmp.setPassword("0000");
-            newEmp.setRole("user");
-            em.persist(newEmp);
-            transaction.commit();
-            return Response.ok().build();
-        } catch (Exception e) {
-            return Response.serverError().
-                    entity(e).
-                    build();
-        }
+    public Employee[] addEmp() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException, NamingException {
+        Employee newEmp = new Employee();
+        newEmp.setName("Anwar");
+        newEmp.setPassword("0000");
+        newEmp.setRole("user");
+//        UserTransaction transaction = (UserTransaction)new InitialContext().lookup("java:comp/UserTransaction");
+//        transaction.begin();
+//        em.getTransaction().begin();
+//        em.createNativeQuery("INSERT INTO USERS (name, password, role) VALUES ('Anwar', '0000', 'user')").executeUpdate();
+        em.persist(newEmp);
+//        em.close();
+//        em.getTransaction().commit();
+//        transaction.commit();
+        return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
     }
 
     @GET
     @Path("admin/{id}")
     @Transactional
-    public Response modifyEmp(@PathParam("id") int id) {
+    public Employee[] modifyEmp(@PathParam("id") int id) {
         try {
-            //em.joinTransaction();
-            return Response.ok().
-                    entity(em.createQuery("UPDATE Employee e SET e.name = 'XY1234' WHERE e.id = :id").setParameter("id", id).executeUpdate()).
-                    build();
+            em.createQuery("UPDATE Employee e SET e.name = 'Mariam' WHERE e.id = :id").setParameter("id", id).executeUpdate();
+            return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
         } catch (Exception e) {
-            return Response.serverError().
-                    entity(e).
-                    build();
+            return new Employee[0];
         }
     }
 
     @GET
     @Path("admin/delete/{id}")
     @Transactional
-    public Response deleteEmp(@PathParam("id") int id) {
+    @Rollback(true)
+    public Employee[] deleteEmp(@PathParam("id") int id) {
         try {
-            //em.joinTransaction();
-            return Response.ok().
-                    entity(em.createQuery("DELETE FROM Employee e WHERE e.id = :id").setParameter("id", id)).
-                    build();
+
+            Employee employee = em.find(Employee.class, id);
+            em.remove(employee);
+
+            return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
         } catch (Exception e) {
-            return Response.serverError().
-                    entity(e).
-                    build();
+            return new Employee[0];
         }
     }
 }
